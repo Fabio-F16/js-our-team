fb3623c7574ce0a270bf89a92453b1d0f6b318f5
Fabio-F16 <fipsfabio95@gmail.com>
2022-03-22 16:23:15 +0100

    post correzione

--- script.js
+++ script.js
@@ -1,77 +1,123 @@
 // console.log('js ok');
 
 
+
+// creazione struttura dati
 const team = [
-    utente1 = {
+    {
         nome: 'Angela Caroll',
         ruolo: 'Chief Editor',
         foto: 'img/angela-caroll-chief-editor.jpg',
     },
-    utente2 = {
+    {
         nome: 'Angela Lopez',
         ruolo: 'Social Media Manager',
         foto: 'img/angela-caroll-chief-editor.jpg',
     },
-    utente3 = {
+    {
         nome: 'Scott Estrada',
         ruolo: 'Developer',
         foto: 'img/angela-caroll-chief-editor.jpg',
     },
-    utente4 = {
+    {
         nome: 'Barbara Ramos',
         ruolo: 'Graphic Designer',
         foto: 'img/angela-caroll-chief-editor.jpg',
     },
-    utente5 = {
+    {
         nome: 'Wayne Barnett',
         ruolo: 'Ceo and Founder',
         foto: 'img/angela-caroll-chief-editor.jpg',
     },
-    utente6 = {
+    {
         nome: 'Walter Gordon',
         ruolo: 'Office Manager',
         foto: 'img/angela-caroll-chief-editor.jpg',
     }
 ];
 
-console.log(team);
-console.log(team[0].nome);
 
 
-for (let i = 0; i < team.length; i++) {
+const teamContainer = document.querySelector('.team-container');
 
-    const teamContainer = document.querySelector('.team-container');
+// al click sul button viene aggiunto un nuovo membro
+const addButton = document.getElementById('addMemberButton');
 
-    // creo elementi dom
-    const teamCard = document.createElement('div');
-    const cardImage = document.createElement('div');
-    const cardText = document.createElement('div');
-    const imgElement = document.createElement('img');
-    const nameUser = document.createElement('h3');
-    const task = document.createElement('p');
+addButton.addEventListener('click', () => addMember(team));
 
 
-    // aggiungo classi
-    teamCard.className = 'team-card';
-    cardImage.className = 'card-image';
-    cardText.className = 'card-text';
+createTeamCards(team);
 
-    // inserimento valori card
-    imgElement.src = team[i].foto;
 
-    nameUser.innerText = team[i].nome;
 
-    task.innerText = team[i].ruolo;
 
-    // inserimento nell'html
-    teamContainer.appendChild(teamCard);
-    teamCard.appendChild(cardImage);
-    teamCard.appendChild(cardText);
-    cardImage.appendChild(imgElement);
-    cardText.appendChild(nameUser);
-    cardText.appendChild(task);
+
+// FUNZIONI -------------------------------------------------------------------------------------------
+
+
+
+const getValue = id => document.getElementById(id).value;
+
+
+/**
+ * Create a card for each member of the team
+ * @param members - an array of objects that contain the member's name, title, and image.
+ */
+function createTeamCards(members) {
+    teamContainer.innerHTML = ''
+    for (let i = 0; i < members.length; i++) {
+
+        const card = createCard(members[i]);
+        teamContainer.appendChild(card);
+
+    }
 
 }
+
+/**
+ * It creates a card for each team member.
+ * @param member - The member object that we want to create a card for.
+ * @returns A card with the team's name, role and photo.
+ */
+function createCard(member) {
+
+    const card = document.createElement('div');
+    card.className = 'team-card';
+
+    card.innerHTML = `
+            <div class="team-card">
+            <div class="card-image">
+                <img src="${member.foto}" alt="${member.nome}" />
+            </div>
+            <div class="card-text">
+                <h3>${member.nome}</h3>
+                <p>${member.ruolo}</p>
+            </div>
+    `;
+
+    return card;
+}
+
+
+
+
+function addMember(data) {
+
+    const nome = getValue('name');
+    const ruolo = getValue('role');
+    const foto = getValue('image');
+
+    const newMember = {
+        nome,
+        ruolo,
+        foto
+    }
+    data.push(newMember);
+
+    createTeamCards(data);
+
+}
+
 
 
 
